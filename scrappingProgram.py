fedca3cd88f598536b1d31dd82e8eefe2056e5f7
ajmette <amette@siue.edu>
2024-10-20 15:35:54 -0500

    getting to review page from item page

--- scrappingProgram.py
+++ scrappingProgram.py
@@ -1,33 +1,58 @@
 import requests
 from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+# Set Chrome options
+chrome_options = Options()
+chrome_options.add_argument("--headless")  # Run Chrome in headless mode
+chrome_options.add_argument("--no-sandbox")  # Optional: Required for certain Linux setups
+chrome_options.add_argument("--disable-dev-shm-usage")  # Optional: Helps with memory issues
 
 with open("urls.txt", 'r') as input:         
     urls = input.readlines()
 
 count = 0
 for url in urls:
-    r = requests.get(url)
+
+    driver = webdriver.Chrome(options=chrome_options)
+    driver.get(url)
+
+    # Get the page source after all JavaScript has loaded
+    html = driver.page_source
+
+    #r = requests.get(url)
+    soup = BeautifulSoup(html, 'html.parser')
+    #feedback1 = soup.find('div', class_ = "d-stores-info-categories__details-container")
+    #feedback = feedback1.find('div', class_ = "tabs__content")
+    #fb = feedback.find('div', class_ = "tabs__cell")
+    #print (soup.prettify())
+    
+    all_page = soup.find('a', class_ = "fdbk-detail-list__tabbed-btn fake-btn fake-btn--large fake-btn--secondary")
+    print(all_page['href'])
+    r = requests.get(str(all_page['href']))
     soup = BeautifulSoup(r.content, 'html.parser')
+
     #ul = soup.find('ul', {'class': 'fdbk-detail-list__cards'})
     t="" 
     
     tabs = soup.find('div', class_ = "tabs__content")
-    cur = tabs.find('div', class_ = "tabs__panel")
-    rws = cur.find_all('div', class_ = "fdbk-container__details__comment")
+    cur_page = tabs.find('div', class_ = "tabs__panel")
+    rws = cur_page.find_all('div', class_ = "fdbk-container__details__comment")
     for r in rws:
         t += r.get_text() + "\n\n"
-    next = cur.find('a', class_ = "pagination__next icon-link")
+    next_page = cur_page.find('a', class_ = "pagination__next icon-link")
     #print(str(count) + ": " )
 
-    while (next != None): 
+    while (next_page != None): 
         #print("1 ")
-        next_url = "https://www.ebay.com/fdbk/mweb_profile" + next['href']
+        next_url = "https://www.ebay.com/fdbk/mweb_profile" + next_page['href']
         r = requests.get(next_url)
         soup = BeautifulSoup(r.content, 'html.parser')
         tabs = soup.find('div', class_ = "tabs__content")
-        cur = tabs.find('div', class_ = "tabs__panel")
-        next = cur.find('a', class_ = "pagination__next icon-link")
-        rws = cur.find_all('div', class_ = "fdbk-container__details__comment")
+        cur_page = tabs.find('div', class_ = "tabs__panel")
+        next_page = cur_page.find('a', class_ = "pagination__next icon-link")
+        rws = cur_page.find_all('div', class_ = "fdbk-container__details__comment")
         for r in rws:
             t += r.get_text() + "\n\n"
 
