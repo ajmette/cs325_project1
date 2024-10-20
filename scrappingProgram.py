4c77f6068cb22e8c9c53810d6300f32052c911ac
ajmette <amette@siue.edu>
2024-10-19 22:19:31 -0500

    trying multiple pages of reviews

--- scrappingProgram.py
+++ scrappingProgram.py
@@ -8,13 +8,40 @@ count = 0
 for url in urls:
     r = requests.get(url)
     soup = BeautifulSoup(r.content, 'html.parser')
-    ul = soup.find('ul', {'class': 'fdbk-detail-list__cards'})
+    #ul = soup.find('ul', {'class': 'fdbk-detail-list__cards'})
+    t="" 
+    
+    tabs = soup.find('div', class_ = "tabs__content")
+    cur = tabs.find('div', class_ = "tabs__panel")
+    rws = cur.find_all('div', class_ = "fdbk-container__details__comment")
+    for r in rws:
+        t += r.get_text() + "\n\n"
+    next = cur.find('a', class_ = "pagination__next icon-link")
+    #print(str(count) + ": " )
 
-    if ul:
+    while (next != None): 
+        #print("1 ")
+        next_url = "https://www.ebay.com/fdbk/mweb_profile" + next['href']
+        r = requests.get(next_url)
+        soup = BeautifulSoup(r.content, 'html.parser')
+        tabs = soup.find('div', class_ = "tabs__content")
+        cur = tabs.find('div', class_ = "tabs__panel")
+        next = cur.find('a', class_ = "pagination__next icon-link")
+        rws = cur.find_all('div', class_ = "fdbk-container__details__comment")
+        for r in rws:
+            t += r.get_text() + "\n\n"
+
+
+    with open("reviews" + str(count)+".txt", "w", encoding="utf-8") as reviews:
+                reviews.write(t)
+    count +=1
+    #print("done\n")
+    
+    '''if ul:
         with open("reviews" +str(count)+".txt", "w", encoding="utf-8") as reviews:  
             for elem in ul.find_all(class_ = "fdbk-container__details__comment"):   
                 reviews.write(elem.get_text() + "\n\n") 
 
-        count +=1
+        
     else:
-        print(f"No 'ul' element found for {url}")
+        print(f"No 'ul' element found for {url}")'''
