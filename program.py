ae3ddf818d1e13338a5da8f246a8b2e25013e267
ajmette <amette@siue.edu>
2024-11-23 13:15:35 -0600

    adding program

--- program.py
+++ program.py
@@ -2,12 +2,15 @@ from modules import Get_Reviews, Get_Sentiments, Plot_Combined_Graph
 import os
 from config import reviews_folder_path, sentiments_folder_path, product_instances
 
+# CHANGE folder_path VARIABLES IN config.py FILE
+
 if __name__ == "__main__":
-    Get_Reviews("urls.txt", reviews_folder_path)
+    Get_Reviews("urls.txt", reviews_folder_path)                    # Scrapes all reviews from each url in the specified file,
+                                                                    # Gives text files of reviews stored in specified folder path
 
     count = 1
-    #Get_Sentiments()
-    for filename in os.listdir(reviews_folder_path):                # Get sentiments for all reviews of each file in the reviews folder
+
+    for filename in os.listdir(reviews_folder_path):                # Get sentiments for all reviews of each file in the specified folder
         file_path = os.path.join(reviews_folder_path, filename)
         if os.path.isfile(file_path):
             product_name = (product_instances[count - 1]).product_name
