dc50ea9c33782189b0f90557fafff965b2eb57c9
ajmette <amette@siue.edu>
2024-11-24 11:44:25 -0600

    adding config

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 # ----------------- CHANGE TO OWN FOLDER PATHS, FOLDER NEEDS TO EITHER NOT ALREADY EXIST OR NEEDS TO BE EMPTY -----------------
-reviews_folder_path = "C:/Users/ajmet/OneDrive/Desktop/CS325/project3/review3/"
-sentiments_folder_path = "C:/Users/ajmet/OneDrive/Desktop/CS325/project3/sentiment3/"
+reviews_folder_path = "C:/Users/ajmet/OneDrive/Desktop/CS325/project3/review/"
+sentiments_folder_path = "C:/Users/ajmet/OneDrive/Desktop/CS325/project3/sentiment/"
 test_folder_path = "C:/Users/ajmet/OneDrive/Desktop/CS325/project3/test"
 product_instances = []
 count = 1
